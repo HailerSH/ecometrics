1f208de36a361d1b513f3e5b92a94a94c07dabfc
HailerSH <hsernah.dev@gmail.com>
2025-11-24 09:26:29 -0500

    feat: add ArchitectureCardComponent with detailed architecture overview

---
+++ frontend/src/app/components/home/architecture-card/architecture-card.component.ts
@@ -0,0 +1,10 @@
+
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-architecture-card',
+  standalone: true,
+  templateUrl: './architecture-card.component.html',
+})
+export class ArchitectureCardComponent {}
