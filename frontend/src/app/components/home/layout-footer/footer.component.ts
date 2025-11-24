53fc9f3510bddaf421193edf592f58d9d9133c8d
HailerSH <hsernah.dev@gmail.com>
2025-11-24 08:11:31 -0500

    feat: add FooterComponent with template for layout footer

---
+++ frontend/src/app/components/home/layout-footer/footer.component.ts
@@ -0,0 +1,11 @@
+
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+
+
+@Component({
+  selector: 'app-footer',
+  standalone: true,
+  templateUrl: './footer.component.html',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+})
+export class FooterComponent {}
