aa6f15bcd3b177afe1c58fc7b6cb5b70f7f8f7c7
HailerSH <hsernah.dev@gmail.com>
2025-11-24 11:54:55 -0500

    feat: add fields for activity, country, and emission_type to AnnualEmission model and create migration

--- backend/emissions/models.py
+++ backend/emissions/models.py
@@ -3,5 +3,25 @@ from django.db import models
 
 
 class AnnualEmission(models.Model):
+
     year = models.IntegerField()
+
     emissions = models.FloatField()
+
+    emission_type = models.CharField(
+        default="",
+        max_length=255
+    )
+
+    country = models.CharField(
+        default="",
+        max_length=255
+    )
+
+    activity = models.CharField(
+        default="",
+        max_length=255
+    )
+
+    def __str__(self):
+        return f"{self.year} | {self.country} | {self.emission_type}"
