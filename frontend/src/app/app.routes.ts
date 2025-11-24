005b8e4ee6e116620cd847fc6b7eb4dcde23bf6e
HailerSH <hsernah.dev@gmail.com>
2025-11-24 14:22:49 -0500

    fix: remove obsolete Technical Breakdown page route and button in links component

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,12 +3,10 @@ import { Routes } from '@angular/router';
 import { HomePageComponent } from '@pages/home/home.page';
 import { ExplorerPageComponent } from '@pages/explorer/explorer.page';
 import { ComparePageComponent } from '@pages/compare/compare.page';
-import { AboutTechnicalPageComponent } from '@pages/about-technical/about-technical.page';
 
 
 export const routes: Routes = [
   { path: '', component: HomePageComponent },
   { path: 'explorer', component: ExplorerPageComponent },
-  { path: 'compare', component: ComparePageComponent },
-  { path: 'about-technical', component: AboutTechnicalPageComponent }
+  { path: 'compare', component: ComparePageComponent }
 ];
